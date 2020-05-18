f4286879a6f1d69d2b31150f5787c7fa0a8d5949
Brian Lipsett <lipsettbrian3@gmail.com>
2020-05-17 21:58:14 -0400

    Updates

---
+++ precache-manifest.2e4aac4595f92355b63e2dd8cadf5df0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffbff626cc6e82e0c12d9afa3c718bed",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "5dd25255d4b9d2a30787",
+    "url": "/reactportfolio/static/css/main.b019a75d.chunk.css"
+  },
+  {
+    "revision": "bcee273f1a4d8b1c9693",
+    "url": "/reactportfolio/static/js/2.128247cd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactportfolio/static/js/2.128247cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dd25255d4b9d2a30787",
+    "url": "/reactportfolio/static/js/main.57f6d6df.chunk.js"
+  },
+  {
+    "revision": "19187e8b15ed8bf611bb",
+    "url": "/reactportfolio/static/js/runtime-main.0f391e1c.js"
+  },
+  {
+    "revision": "0a99c43e4fe9a8fd199408f8a08f00ee",
+    "url": "/reactportfolio/static/media/Group2.0a99c43e.svg"
+  },
+  {
+    "revision": "ff1802b4c1a3b5a9315c86a8693242a4",
+    "url": "/reactportfolio/static/media/blemProf-min.ff1802b4.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30e48114990ff07d78bf8612f6b40220.js"
+  "/reactportfolio/precache-manifest.2e4aac4595f92355b63e2dd8cadf5df0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactportfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
